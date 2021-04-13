b59f5cda263de41e705e8c50e03792f089b6387b
Jaebock jo <last2599@gmail.com>
2021-04-13 22:26:11 +0900

    django-admin startapp frontend

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 # Create your views here.
 
 # /hello
 # /hi
 
-def main(request):
-    return HttpResponse("Hello")
+# def main(request):
+#     return HttpResponse("Hello")
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
